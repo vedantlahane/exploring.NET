ec4bbeafda4db628c271cdb151cce44d38e2d711
Vedant Lahane <vedantanillahane@gmail.com>
2025-12-18 16:46:16 +0530

    Add LargestNumber01 class to determine the largest of three numbers

---
+++ largest_number_01.cs
@@ -0,0 +1,15 @@
+
+
+class LargestNumber01
+{
+    public static void Main(string[] args)
+    {
+        int a = 10, b = 25 , c= 8;
+        if(a > b && a > c)
+            Console.WriteLine($"Largest: {a}");
+        else if(b > c)
+            Console.WriteLine($"Largest: {b}");
+        else
+            Console.WriteLine($"Largest: {c}");
+    }
+}
