f3f134793808564f5deb82857f5af72d494220ec
Vedant Lahane <vedantanillahane@gmail.com>
2025-12-20 06:42:35 +0530

    Refactor MathOperations class: standardize method names and improve formatting

--- MathOperations03.cs
+++ MathOperations03.cs
@@ -1,18 +1,30 @@
-using System;
 class MathOperations
 {
-    public int add(int a,int b)
+    public static int Add(int a, int b)
     {
-        return a+b;
+        return a + b;
     }
-    public double add(double a,double b)
+    public static double Add(double a, double b)
     {
-        return a+b;
+        return a + b;
     }
 
-
-    public static void Main(String[] args)
+    public static int Sum(params int[] num)
     {
-        
+        int total = 0;
+        foreach (int a in num)
+        {
+            total += a;
+        }
+        return total;
     }
-}        
+
+    public static void Main(string[] args)
+    {
+        string name = "Vedant";
+        Console.WriteLine($"Hello, {name}!");
+        Console.WriteLine($"Add ints: {Add(1, 2)}");
+        Console.WriteLine($"Add doubles: {Add(1.5, 2.5)}");
+        Console.WriteLine($"Sum: {Sum(1, 2, 3, 4, 5)}");
+    }
+}
