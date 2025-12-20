8c58febc1f5fc9b1425e5f482bbea4532ff84835
Vedant Lahane <vedantanillahane@gmail.com>
2025-12-20 06:09:20 +0530

    Add MathOperations class with methods for integer and double addition

---
+++ MathOperations03.cs
@@ -0,0 +1,18 @@
+using System;
+class MathOperations
+{
+    public int add(int a,int b)
+    {
+        return a+b;
+    }
+    public double add(double a,double b)
+    {
+        return a+b;
+    }
+
+
+    public static void Main(String[] args)
+    {
+        
+    }
+}        
